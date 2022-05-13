ddbbeddf000b6465ec9b2e26660ee65cb14e0e7e
Aatmodhee Goswami <atomind2005@gmail.com>
2022-05-13 16:29:52 -0500

    Added the basic attributes this project is to be based on

---
+++ Hypergraph.h
@@ -0,0 +1,18 @@
+#ifndef HYPERGRAPH_H
+#define HYPERGRAPH_H
+#include <iostream>
+#include <vector>
+#include <queue>
+#include <string>
+
+class Hypergraph{
+	private:
+	vector<vector<int>> edgeLists;
+	vector<int> adjVertices;
+	vector<string> adjEdges;
+	public:
+		
+}
+
+#endif
+
