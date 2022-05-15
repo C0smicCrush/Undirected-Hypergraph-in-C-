9bdfbfda43cf52c0daee67335cd0cd8791400ba7
Aatmodhee Goswami <atomind2005@gmail.com>
2022-05-15 02:45:36 -0500

    addEdge is now functional :D

--- Hypergraph.cpp
+++ Hypergraph.cpp
@@ -8,7 +8,10 @@ bool Hypergraph::addEdge(int edgeAdd){
 	if(!std::binary_search (adjEdges.begin(), adjEdges.end(), edgeAdd) && edgeAdd > 0){
 		added = 1;
 		int loc  = getInputLocation(edgeAdd, 0);
+		vector<int> test = {};
 		adjEdges.insert(adjEdges.begin()+loc, edgeAdd);
+		
+		edgeLists.insert(edgeLists.begin()+loc, test);
 		
 	}
 	return added;
@@ -18,28 +21,40 @@ bool Hypergraph::addEdge(int edgeAdd){
 int Hypergraph::getInputLocation(int intIn, int check){   //Second integer determines if its for edges or vertices
 													  // 0 is for edges and 1 is for vertices
 	int ret = 0;//ret is the location to be returned
-//	if(check == 0){
-//		if(adjEdges.size() > 0){
-//			for(int x = 0; x < edges; x++){
-//				if(adjEdges[x] < 0){
-//					ret++;
-//				}
-//			}
-//		}
-//	}
-//	else{
-//		if(adjVertices.size() > 0){
-//			for(int x = 0; x < edges; x++){
-//				if(adjEdges[x] < 0){
-//					ret++;
-//				}
-//			}
-//		}
-//	}
+	if(check == 0){
+		if(adjEdges.size() > 0){
+			for(int x = 0; x < edges; x++){
+				if(adjEdges[x] < 0){
+					ret++;
+				}
+			}
+		}
+	}
+	else{
+		if(adjVertices.size() > 0){
+			for(int x = 0; x < edges; x++){
+				if(adjEdges[x] < 0){
+					ret++;
+				}
+			}
+		}
+	}
 	return ret;
 	
 }
+void Hypergraph::testfunc(){
+	std::cout << edgeLists.size();
+	std::cout << edgeLists[0].size();
+}
 
+
+
+//void Hypergraph::printEdge(int edgeIn){
+//	if (edgeIn > 0){
+//		int loc = getInputLocation(edgeIn,0);
+//		for(int x =0; x < );
+//	}
+//}
 
 //int Hypergraph::binarySearch(int intIn, int checker){ //Second integer determines if its for edges or vertices
 //													  // 0 is for edges and 1 is for vertices
