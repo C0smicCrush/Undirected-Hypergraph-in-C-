aa282039306216bf81e27f23ee5e33b161bb1a52
Aatmodhee Goswami <atomind2005@gmail.com>
2022-05-14 20:59:14 -0500

    Fixed the segmentation faults and made the search for the element's Big-O into log 2 n instead of n

--- Hypergraph.cpp
+++ Hypergraph.cpp
@@ -5,33 +5,42 @@ Hypergraph::Hypergraph(){
 }
 bool Hypergraph::addEdge(int edgeAdd){
 	bool added = 0;
+	if(!std::binary_search (adjEdges.begin(), adjEdges.end(), edgeAdd) && edgeAdd > 0){
+		added = 1;
+		int loc  = getInputLocation(edgeAdd, 0);
+		adjEdges.insert(adjEdges.begin()+loc, edgeAdd);
+		
+	}
+	return added;
 	
 	
 }
 int Hypergraph::getInputLocation(int intIn, int check){   //Second integer determines if its for edges or vertices
 													  // 0 is for edges and 1 is for vertices
 	int ret = 0;//ret is the location to be returned
-	if(check == 0){
-		if(adjEdges.size() > 0){
-			for(int x = 0; x < edges; x++){
-				if(adjEdges[x] < 0){
-					ret++;
-				}
-			}
-		}
-	}
-	else{
-		if(adjVertices.size() > 0){
-			for(int x = 0; x < edges; x++){
-				if(adjEdges[x] < 0){
-					ret++;
-				}
-			}
-		}
-	}
+//	if(check == 0){
+//		if(adjEdges.size() > 0){
+//			for(int x = 0; x < edges; x++){
+//				if(adjEdges[x] < 0){
+//					ret++;
+//				}
+//			}
+//		}
+//	}
+//	else{
+//		if(adjVertices.size() > 0){
+//			for(int x = 0; x < edges; x++){
+//				if(adjEdges[x] < 0){
+//					ret++;
+//				}
+//			}
+//		}
+//	}
 	return ret;
 	
 }
+
+
 //int Hypergraph::binarySearch(int intIn, int checker){ //Second integer determines if its for edges or vertices
 //													  // 0 is for edges and 1 is for vertices
 //	int left = 0;
