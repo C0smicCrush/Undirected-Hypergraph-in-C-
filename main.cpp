230bb879c20ce6f167b4cb53015d1d678999f853
Aatmodhee Goswami <atomind2005@gmail.com>
2022-08-07 12:03:48 -0500

    Testing Methods

--- main.cpp
+++ main.cpp
@@ -1,6 +1,13 @@
 #include "main.h"
 int main(){
 	Hypergraph hyper;
-	hyper.addEdge(1);
+	std::cout << hyper.addEdge(1);
+	std::cout << hyper.addEdge(1);
+
+	std::cout << hyper.addVertex(1);
+	std::cout << hyper.addVertex(1);
+
+	std::cout << hyper.addVertextoEdge(1,1);
+	std::cout << hyper.addVertextoEdge(1,1);
 	return 0;
 }
